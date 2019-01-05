b808e32524c421d3c14f92c187eb88d3cac9fcff
Josh Pigford <josh@joshpigford.com>
2019-01-05 17:25:17 -0600

    Reduce member options

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -5,7 +5,7 @@ class SkillsController < ApplicationController
   def show
     skill = Skill.ensure!(params[:id])
     band = Band.ensure!(params[:band_id])
-    members = Member.with_skill(skill).limit(12).order(Arel.sql("RANDOM()"))
+    members = Member.with_skill(skill).limit(8).order(Arel.sql("RANDOM()"))
 
     render :show, locals: {skill: skill, band: band, members: members}
   end
