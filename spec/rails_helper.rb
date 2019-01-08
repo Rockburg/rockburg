0f1bc984ebd50e07bd9030858473c2aa442bd180
Nathan Palmer <nathan@nathanpalmer.com>
2019-01-07 22:02:40 -0500

    Removing this since I don't think it was necessary

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,6 @@ require_relative '../lib/lastfm.rb'
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'devise'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'sidekiq/testing'
 require 'capybara/rspec'
