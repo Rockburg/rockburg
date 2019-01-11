c2c25cee383a22a40f84302833fc51505a98a730
Nathan Palmer <nathan@nathanpalmer.com>
2019-01-10 21:48:01 -0500

    Adding the charts controller specs

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,19 +1,23 @@
 class ChartsController < ApplicationController
+  skip_after_action :verify_authorized
+  after_action :verify_policy_scoped
+
   def index
-    @managers = Manager.with_bands.order(balance: :desc).limit(5)
-    @bands = Band.order(buzz: :desc).joins(:manager).limit(5)
-    @releases = Recording.released.order(sales: :desc).limit(5)
+    @managers = policy_scope(Manager).with_bands.order(balance: :desc).limit(5)
+    @bands = policy_scope(Band).order(buzz: :desc).joins(:manager).limit(5)
+    @releases = policy_scope(Recording).released.order(sales: :desc).limit(5)
   end
 
   def bands
-    @bands = Band.order(buzz: :desc).limit(30)
+    @bands = policy_scope(Band).order(buzz: :desc).limit(30)
   end
 
   def managers
-    @managers = Manager.with_bands.order(balance: :desc).limit(30)
+    @managers = policy_scope(Manager).with_bands.order(balance: :desc).limit(30)
   end
 
   def releases
+    skip_policy_scope
     #@releases = Release.join(:streams).order(num_streams: :desc).limit(30)
   end
 end
