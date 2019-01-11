4e2c22897c84ff8f597f13c571dffe008dd2e228
Nathan Palmer <nathan@nathanpalmer.com>
2019-01-10 21:48:09 -0500

    Fixing the recordings factory

--- spec/factories/recordings.rb
+++ spec/factories/recordings.rb
@@ -1,12 +1,32 @@
+# == Schema Information
+#
+# Table name: recordings
+#
+#  id         :bigint(8)        not null, primary key
+#  quality    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  band_id    :bigint(8)
+#  song_id    :bigint(8)
+#  studio_id  :bigint(8)
+#
+# Indexes
+#
+#  index_recordings_on_band_id    (band_id)
+#  index_recordings_on_song_id    (song_id)
+#  index_recordings_on_studio_id  (studio_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (song_id => songs.id)
+#
+
 FactoryBot.define do
   factory :recording do
     association :studio
     association :band
+    association :song
 
-    kind { :single }
-    name { Faker::FunnyName.name }
     quality { rand(40..100) }
-    release_at { Faker::Date.forward(5.minutes) }
-    sales { 0 }
   end
 end
