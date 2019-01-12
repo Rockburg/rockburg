81b5b61ca74d129910b1c1b8987fa5837ebf8664
Nathan Palmer <nathan@nathanpalmer.com>
2019-01-12 15:54:43 -0500

    Removing all the comments in the test and service

--- spec/services/activity/release_recording_spec.rb
+++ spec/services/activity/release_recording_spec.rb
@@ -21,18 +21,11 @@ RSpec.describe Activity::ReleaseRecording, type: :service do
     end
 
     it 'should release an album' do
-      # balance = band.manager.balance
-
       Sidekiq::Testing.inline! do
         result = described_class.call(band: band, recording_ids: band.recordings.ids, release_name: Faker::FunnyName.name, release_kind: :album)
         expect(result.success?).to eq(true)
         expect(result.activity).to be_present
       end
-
-      # TODO: Revisit comments in Band::ReleaseRecording
-      # expect(album.release_at).not_to be_nil
-      # expect(album.sales).to be > sales
-      # expect(balance).to be > band.manager.balance
     end
   end
 
@@ -48,24 +41,11 @@ RSpec.describe Activity::ReleaseRecording, type: :service do
     end
 
     it 'should release a single' do
-      # balance = band.manager.balance
-      # recording = band.recordings.singles.first
-      # expect(recording).not_to be_nil
-      # expect(recording.release_at).to be_nil
-      # sales = recording.sales
-
       Sidekiq::Testing.inline! do
         result = described_class.call(band: band, recording_ids: [band.recordings.ids.first], release_name: Faker::FunnyName.name, release_kind: :single)
         expect(result.success?).to eq(true)
         expect(result.activity).to be_present
       end
-
-      # TODO: Revisit comments in Band::ReleaseRecording
-      # recording.reload
-      # band.manager.reload
-      # expect(recording.release_at).not_to be_nil
-      # expect(recording.sales).to be > sales
-      # expect(balance).to be > band.manager.balance
     end
   end
 
