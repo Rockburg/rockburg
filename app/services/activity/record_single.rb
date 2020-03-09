e70094416e38f8c56d648f323b1e28697ec23c0e
Josh Pigford <josh@joshpigford.com>
2020-03-09 16:12:24 -0500

    I can't make up my mind

--- app/services/activity/record_single.rb
+++ app/services/activity/record_single.rb
@@ -21,8 +21,14 @@ class Activity::RecordSingle < ApplicationService
     end_at = start_at + hours * ENV["SECONDS_PER_GAME_HOUR"].to_i
     context.activity = Activity.create!(band: band, action: :record_single, starts_at: start_at, ends_at: end_at)
 
+    release_activity = Activity.create!(band: band, action: :release, starts_at: start_at, ends_at: end_at)
+
     recording = band.recordings.create(studio: studio, song: song)
 
+    release = band.releases.create!(name: song.name, kind: 'single')
+
     Band::RecordSingleWorker.perform_at(end_at, band.to_global_id, recording.to_global_id, hours, context.activity.id)
+
+    Band::ReleaseWorker.perform_at(end_at+1.second, band.to_global_id, release.to_global_id, release_activity.id)
   end
 end
